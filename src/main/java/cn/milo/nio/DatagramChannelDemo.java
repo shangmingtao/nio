082f5ed1fd039c38be7c5c4672d55ce029048126
shangmingtao <shangmingtao@126.com>
2018-01-23 14:12:59 +0800

    Update DatagramChannelDemo.java

--- src/main/java/cn/milo/nio/DatagramChannelDemo.java
+++ src/main/java/cn/milo/nio/DatagramChannelDemo.java
@@ -10,7 +10,7 @@ import java.nio.channels.DatagramChannel;
 public class DatagramChannelDemo {
 
     public static void main(String[] args)throws Exception {
-        //create22
+        //create111
         DatagramChannel channel = DatagramChannel.open();
         channel.socket().bind(new InetSocketAddress("127.0.0.1",8888));
 
